85fd699cfe0bccffe53f44ffdf9c244e33e36f24
katyabb <ekater1nabiruk@yandex,ru>
2022-10-28 01:35:48 +0300

    add project

---
+++ EmployeesAndSalaries/AppConfiguration/ServicesExtensions/AddVersioningConfiguration.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesAndSalaries.AppConfiguration.ServicesExtensions
+{
+
+    public static partial class ServicesExtensions
+    {
+
+        public static void AddVersioningConfiguration(this IServiceCollection services)
+        {
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+        }
+    }
+}
