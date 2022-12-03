2eaa3b9e8b2c63f9566cb3f7f4dc46c7ed9a748a
katyabb <ekater1nabiruk@yandex,ru>
2022-12-03 12:58:52 +0300

    add 3rd

---
+++ Employees.Entities/Migrations/20221203095214_InitialMigration.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Employees.Entities.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PassportSerial = table.Column<int>(type: "int", nullable: false),
+                    PassportNumber = table.Column<int>(type: "int", nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Snils = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModificationTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "jobtitles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    JobTitleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModificationTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_jobtitles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "educations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NameOfOrganization = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearOfEnding = table.Column<int>(type: "int", nullable: false),
+                    LevelOfEducation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AcademicDegree = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeerID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModificationTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_educations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_educations_employees_EmployeerID",
+                        column: x => x.EmployeerID,
+                        principalTable: "employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "salaries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BasicSalary = table.Column<int>(type: "int", nullable: false),
+                    Premium = table.Column<int>(type: "int", nullable: false),
+                    Month = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    WorkDays = table.Column<int>(type: "int", nullable: false),
+                    EmployeerID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModificationTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_salaries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_salaries_employees_EmployeerID",
+                        column: x => x.EmployeerID,
+                        principalTable: "employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "experiences",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExperienceTime = table.Column<int>(type: "int", nullable: false),
+                    JobTitleID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmployeerID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModificationTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_experiences", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_experiences_employees_EmployeerID",
+                        column: x => x.EmployeerID,
+                        principalTable: "employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_experiences_jobtitles_JobTitleID",
+                        column: x => x.JobTitleID,
+                        principalTable: "jobtitles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_educations_EmployeerID",
+                table: "educations",
+                column: "EmployeerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_experiences_EmployeerID",
+                table: "experiences",
+                column: "EmployeerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_experiences_JobTitleID",
+                table: "experiences",
+                column: "JobTitleID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_salaries_EmployeerID",
+                table: "salaries",
+                column: "EmployeerID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "educations");
+
+            migrationBuilder.DropTable(
+                name: "experiences");
+
+            migrationBuilder.DropTable(
+                name: "salaries");
+
+            migrationBuilder.DropTable(
+                name: "jobtitles");
+
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
