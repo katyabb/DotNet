80e017aa4d0e47d73a50bbac80c45811228fa4e6
katyabb <ekater1nabiruk@yandex,ru>
2022-12-03 11:26:40 +0300

    add 3rd

---
+++ Employees.Repository/Repository.cs
@@ -0,0 +1,76 @@
+using System.Linq.Expressions;
+using Employees.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Employees.Repository;
+
+public class Repository<T> : IRepository<T> where T : BaseEntity
+{
+    private DbContext _context;
+    private ILogger<Repository<T>> logger;
+
+    public Repository(DbContext context, ILogger<Repository<T>> logger)
+    {
+        _context = context;
+        this.logger = logger;
+    }
+    public void Delete(T obj)
+    {
+        _context.Set<T>().Attach(obj);
+        _context.Entry(obj).State = EntityState.Deleted;
+        _context.SaveChanges();
+    }
+
+    public IQueryable<T> GetAll()
+    {
+        return _context.Set<T>();
+    }
+
+    public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate)
+    {
+        return _context.Set<T>().Where(predicate);
+    }
+
+    public T GetById(Guid id)
+    {
+        return _context.Set<T>().FirstOrDefault(x => x.Id == id);
+    }
+
+    private T Insert(T obj)
+    {
+        obj.Init();
+        var result = _context.Set<T>().Add(obj);
+        _context.SaveChanges();
+        return result.Entity;
+    }
+
+    private T Update(T obj)
+    {
+        obj.ModificationTime = DateTime.UtcNow;
+        var result = _context.Set<T>().Attach(obj);
+        _context.Entry(obj).State = EntityState.Modified;
+        _context.SaveChanges();
+        return result.Entity;
+    }
+
+    public T Save(T obj)
+    {
+        try
+        {
+            if (obj.IsNew())
+            {
+                return Insert(obj);
+            }
+            else
+            {
+                return Update(obj);
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.ToString());
+            throw ex;
+        }
+    }
+}
---
+++ Employees.Repository/ServicesExtensions/AddRepositoryConfiguration.cs
@@ -0,0 +1,14 @@
+using Employees.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Employees.Repository;
+
+public static partial class ServicesExtensions
+{
+    public static void AddRepositoryConfiguration(this IServiceCollection services)
+    {
+        services.AddScoped<DbContext, Context>();
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+    }
+}
